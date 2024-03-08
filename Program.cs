29d1f9242b8304adf130141c0d5b0e77b41de73e
humo_r1st <miklvizh@gmail.com>
2024-03-08 18:05:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int[] massive = new int[30000];
+for (int i = 0; i <massive.Length; i++)
+{
+    Random r = new Random();
+    massive[i] = r.Next(0,30000);
+}
+for (int i = 0;i < massive.Length;i++)
+{
+    for (int j = 0; j < massive.Length - i - 1; j++)
+    {
+        if (massive[j] > massive[j+1])
+        {
+            int saved = massive[j];
+            massive[j] = massive[j+1];
+            massive[j + 1] = saved; 
+
+        }
+    }
+}
+foreach (int num in massive)
+{
+    Console.WriteLine(num);
+}
